732626e5b589b63ece9d973646c39c531974d148
bmacarini <bmacarini@gmail.com>
2020-11-18 08:09:37 -0300

    Challenge done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories')
+    .then(response => {setRepos(response.data)})
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+      const response = await api.post('/repositories', {
+        title: "Frontend com VueJS",
+        url: "https://github.com/bmacarini/desafio-conceitos-reactjs",
+        techs: "VueJS"
+      })
+
+    setRepos([ ...repos, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    await api.delete(`/repositories/${id}`);
+
+    setRepos(repos.filter( repo => repo.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repos.map(repo => (
+        <li key={repo.id}>
+          {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
